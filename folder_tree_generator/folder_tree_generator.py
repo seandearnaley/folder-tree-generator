c7b6bbea12429e6b24577c4b8025e7ff42423b46
Sean Dearnaley <seandearnaley@hotmail.com>
2023-06-09 03:33:13 -0400

    fix strict typing

--- folder_tree_generator/folder_tree_generator.py
+++ folder_tree_generator/folder_tree_generator.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import List, Optional
 
 
-def _parse_ignore_patterns(ignorefile_path: Path) -> List[str]:
+def parse_ignore_patterns(ignorefile_path: Path) -> List[str]:
     """Parse a .gitignore like ignore file and return a list of ignored patterns."""
     with open(ignorefile_path, encoding="utf-8") as file:
         return [
@@ -14,49 +14,49 @@ def _parse_ignore_patterns(ignorefile_path: Path) -> List[str]:
         ]
 
 
-def _is_ignored(entry: Path, ignored_patterns: List[str]) -> bool:
+def is_ignored(entry: Path, ignored_patterns: List[str]) -> bool:
     """Check if a file or folder should be ignored."""
     return entry.name == ".gitignore" or any(
         entry.match(pattern) for pattern in ignored_patterns
     )
 
 
-def _list_entries(root: Path) -> List[Path]:
+def list_entries(root: Path) -> List[Path]:
     """List entries in a directory, sorted by type and name."""
     return sorted(root.iterdir(), key=lambda e: (e.is_file(), e.name.lower()))
 
 
-def _entry_to_string(entry: Path, indent: str, ignored_patterns: List[str]) -> str:
+def entry_to_string(entry: Path, indent: str, ignored_patterns: List[str]) -> str:
     """Convert a file system entry to its string representation."""
     if (
         entry.is_dir()
-        and not _is_ignored(entry, ignored_patterns)
+        and not is_ignored(entry, ignored_patterns)
         and entry.name != ".git"
     ):
-        subtree = _generate_folder_tree(entry, indent + "|   ", ignored_patterns)
+        subtree = generate_folder_tree(entry, indent + "|   ", ignored_patterns)
         return f"{indent}|-- {entry.name}/\n{subtree}"
-    if entry.is_file() and not _is_ignored(entry, ignored_patterns):
+    if entry.is_file() and not is_ignored(entry, ignored_patterns):
         return f"{indent}|-- {entry.name}\n"
 
     return ""
 
 
-def _generate_folder_tree(
+def generate_folder_tree(
     root: Path, indent: str = "", ignored_patterns: Optional[List[str]] = None
 ) -> str:
     """Generate a tree of a folder."""
     if ignored_patterns is None:
         ignored_patterns = []
 
-    entries = _list_entries(root)
+    entries = list_entries(root)
     tree_str = "".join(
-        _entry_to_string(entry, indent, ignored_patterns) for entry in entries
+        entry_to_string(entry, indent, ignored_patterns) for entry in entries
     )
 
     return tree_str
 
 
-def _parse_arguments() -> str:
+def parse_arguments() -> str:
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(
         description="Generate a text representation of a folder."
@@ -79,22 +79,22 @@ def generate_tree(root_folder: str, ignore_file_name: Optional[str] = None) -> s
     root = Path(root_folder)
     ignorefile_path = root / ignore_file_name if ignore_file_name else None
     ignored_patterns = (
-        _parse_ignore_patterns(ignorefile_path)
+        parse_ignore_patterns(ignorefile_path)
         if ignorefile_path and ignorefile_path.exists()
         else []
     )
 
     tree_str = f"{root.name}/\n"
-    tree_str += _generate_folder_tree(root, ignored_patterns=ignored_patterns)
+    tree_str += generate_folder_tree(root, ignored_patterns=ignored_patterns)
     return tree_str
 
 
-def _main() -> None:
+def main() -> None:
     """Main function."""
-    root_folder = _parse_arguments()
+    root_folder = parse_arguments()
     output_text = generate_tree(root_folder, ignore_file_name=".gitignore")
     print(output_text)
 
 
 if __name__ == "__main__":
-    _main()
+    main()
--- tests/test_folder_tree_generator.py
+++ tests/test_folder_tree_generator.py
@@ -2,18 +2,21 @@
 import sys
 from pathlib import Path
 from typing import Union
+from unittest.mock import MagicMock
 
 import pytest
+from pytest import CaptureFixture
+from pytest_mock import MockerFixture
 
 from folder_tree_generator.folder_tree_generator import (
-    _entry_to_string,
-    _generate_folder_tree,
-    _is_ignored,
-    _list_entries,
-    _main,
-    _parse_arguments,
-    _parse_ignore_patterns,
+    entry_to_string,
+    generate_folder_tree,
     generate_tree,
+    is_ignored,
+    list_entries,
+    main,
+    parse_arguments,
+    parse_ignore_patterns,
 )
 
 
@@ -38,21 +41,21 @@ def fixture_sample_directory(tmp_path: Path) -> Path:
 
 def test_parse_ignore_patterns(sample_ignore_file: Path) -> None:
     """Test parsing of .gitignore file."""
-    patterns = _parse_ignore_patterns(sample_ignore_file)
+    patterns = parse_ignore_patterns(sample_ignore_file)
     assert patterns == ["*.txt", "*.log"]
 
 
 def test_is_ignored(sample_directory: Path) -> None:
     """Test if a file or folder should be ignored."""
     ignored_patterns = ["*.txt", "*.log"]
-    assert _is_ignored(sample_directory / "file1.txt", ignored_patterns)
-    assert _is_ignored(sample_directory / "file2.log", ignored_patterns)
-    assert not _is_ignored(sample_directory / "folder1", ignored_patterns)
+    assert is_ignored(sample_directory / "file1.txt", ignored_patterns)
+    assert is_ignored(sample_directory / "file2.log", ignored_patterns)
+    assert not is_ignored(sample_directory / "folder1", ignored_patterns)
 
 
 def test_list_entries(sample_directory: Path) -> None:
     """Test listing of entries in a directory."""
-    entries = _list_entries(sample_directory)
+    entries = list_entries(sample_directory)
     assert entries == [
         sample_directory / "folder1",
         sample_directory / "folder2",
@@ -65,24 +68,22 @@ def test_entry_to_string(sample_directory: Path) -> None:
     """Test conversion of a directory entry to its string representation."""
     ignored_patterns = ["*.txt", "*.log"]
     assert (
-        _entry_to_string(sample_directory / "folder1", "", ignored_patterns)
+        entry_to_string(sample_directory / "folder1", "", ignored_patterns)
         == "|-- folder1/\n"
     )
-    assert _entry_to_string(sample_directory / "file1.txt", "", ignored_patterns) == ""
+    assert entry_to_string(sample_directory / "file1.txt", "", ignored_patterns) == ""
 
 
 def test_generate_folder_tree(sample_directory: Path) -> None:
     """Test generation of a tree of a folder."""
     ignored_patterns = ["*.txt", "*.log"]
-    tree_str = _generate_folder_tree(
-        sample_directory, ignored_patterns=ignored_patterns
-    )
+    tree_str = generate_folder_tree(sample_directory, ignored_patterns=ignored_patterns)
     assert tree_str == "|-- folder1/\n|-- folder2/\n"
 
 
 def test_generate_folder_tree_with_ignore(sample_directory: Path) -> None:
     """Test generation of a tree of a folder."""
-    tree_str = _generate_folder_tree(sample_directory, ignored_patterns=None)
+    tree_str = generate_folder_tree(sample_directory, ignored_patterns=None)
     assert tree_str == "|-- folder1/\n|-- folder2/\n|-- file1.txt\n|-- file2.log\n"
 
 
@@ -97,14 +98,16 @@ def test_generate_tree(sample_directory: Path, sample_ignore_file: Path) -> None
     assert tree_str == expected_tree_str
 
 
-def test_main(sample_directory: Path, sample_ignore_file: Path, capsys) -> None:
+def test_main(
+    sample_directory: Path, sample_ignore_file: Path, capsys: CaptureFixture[str]
+) -> None:
     """Test main function."""
     # Move the sample_ignore_file to the sample_directory
     sample_ignore_file.rename(sample_directory / sample_ignore_file.name)
 
     # Call main with sample_directory
     sys.argv = ["folder_tree_generator.py", str(sample_directory)]
-    _main()
+    main()
 
     # Capture the output
     output = capsys.readouterr().out
@@ -125,7 +128,7 @@ def test_main(sample_directory: Path, sample_ignore_file: Path, capsys) -> None:
 def test_entry_handling_cases(
     sample_directory: Path,
     entry_name: str,
-    ignored_patterns: list,
+    ignored_patterns: list[str],
     expected_result: Union[bool, str],
 ) -> None:
     """Test handling of different entries."""
@@ -137,25 +140,25 @@ def test_entry_handling_cases(
     entry = sample_directory / entry_name
 
     if isinstance(expected_result, bool):
-        assert _is_ignored(entry, ignored_patterns) == expected_result
+        assert is_ignored(entry, ignored_patterns) == expected_result
     else:
-        assert _entry_to_string(entry, "", ignored_patterns) == expected_result
+        assert entry_to_string(entry, "", ignored_patterns) == expected_result
 
 
-def test_parse_arguments(mocker) -> None:
+def test_parse_arguments(mocker: MockerFixture) -> None:
     """Test parsing of command line arguments."""
 
     # Mock the pathlib.Path.is_dir method
-    is_dir_mock = mocker.patch("pathlib.Path.is_dir")
+    is_dir_mock: MagicMock = mocker.patch("pathlib.Path.is_dir")
 
     # Test when the correct number of arguments is provided and root_folder is valid
     mocker.patch("sys.argv", ["script.py", "root_folder"])
     is_dir_mock.return_value = True
-    assert _parse_arguments() == "root_folder"
+    assert parse_arguments() == "root_folder"
 
     # Test when the provided root_folder is not a valid directory
     mocker.patch("sys.argv", ["script.py", "invalid_root_folder"])
     is_dir_mock.return_value = False
     with pytest.raises(ValueError) as exc_info:
-        _parse_arguments()
+        parse_arguments()
     assert str(exc_info.value) == "invalid_root_folder is not a valid directory"
